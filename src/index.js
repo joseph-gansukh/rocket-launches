9dc613dde6a93f7aaff0fa5c1605358e062eee8f
joseph-gansukh <g.sukhbat@gmail.com>
2020-02-29 23:27:13 -0600

    rendering only the flight number, mission name, and launch date" `

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ const fetchData = async () => {
   const res = await fetch("https://api.spacexdata.com/v3/launches")
   const data = await res.json()
   const filterByYear = await data.filter(launch => launch.launch_year === '2020')
-  const launches = await JSON.stringify(filterByYear, null, 2)
+  const newLaunchObj = await filterByYear.map(obj => {
+    return {
+      "flight_number": obj.flight_number,
+      "mission_name": obj.mission_name,
+      "launch_date": new Date(obj.launch_date_utc).toDateString()
+    }
+  })
+  const launches = JSON.stringify(newLaunchObj, null, 2)
+  console.log(launches)
 
   const div = document.getElementById('launches')
-  div.innerHTML = launches
+  div.innerText = launches
 }
 
 document.addEventListener("DOMContentLoaded", event => {
