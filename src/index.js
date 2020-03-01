7ad4b2f562f6317adb649630b8e7095047d30fae
joseph-gansukh <g.sukhbat@gmail.com>
2020-02-29 23:18:24 -0600

    rendering all 2020 launches" "

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@
 const fetchData = async () => {
   const res = await fetch("https://api.spacexdata.com/v3/launches")
   const data = await res.json()
-  // const filterByYear = await data.filter(launch => )
-  console.log("data: ", data)
+  const filterByYear = await data.filter(launch => launch.launch_year === '2020')
+  const launches = await JSON.stringify(filterByYear, null, 2)
+
+  const div = document.getElementById('launches')
+  div.innerHTML = launches
 }
 
-fetchData().catch(alert)
-
-const div = document.getElementById('launches')
 document.addEventListener("DOMContentLoaded", event => {
-  console.log('DOM is loaded')
+
+  fetchData().catch(error => console.log(error))
+
+  
 })
